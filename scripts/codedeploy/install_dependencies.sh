67d329e9261b6bd4eab9c2da3651f28c95805454
aaron-adimverse <aaron@adimverse.com>
2025-02-24 11:36:28 -0700

    feat: updated codedeploy actions for AL2023

--- scripts/codedeploy/install_dependencies.sh
+++ scripts/codedeploy/install_dependencies.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-export PATH="/home/ubuntu/.local/share/pnpm:/home/ubuntu/.nvm/versions/node/v23.3.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:$PATH"
-
 cd ~/atlas
 
 # Install dependencies
-sudo pnpm i --no-frozen-lockfile
-sudo pnpm build
+pnpm i --no-frozen-lockfile
+pnpm build
