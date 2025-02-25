b94dda6827434f7bc91548eb56bf5bee22b77d2a
aaron-adimverse <aaron@adimverse.com>
2025-02-25 15:22:35 -0700

    fix: Correct object path for health check route

--- packages/client-atlas-direct/src/middleware/apiKeyAuth.ts
+++ packages/client-atlas-direct/src/middleware/apiKeyAuth.ts
@@ -4,8 +4,8 @@ const apiKey = process.env.DIRECT_CLIENT_API_KEY as string | undefined
 export function makeApiKeyAuthMiddleware(req: Request, res: Response, next: NextFunction): void {
     const requestKey = req.headers["x-api-key"] as string | undefined
 
-    // If no API key is set in the config, skip checking
-    if (!apiKey || req.route?.path === '/hello') {
+    // If no API key is set in the config, or if the request is against the health check route, skip checking
+    if (!apiKey || req.url === '/hello') {
       next()
       return
     }
