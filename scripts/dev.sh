3fa3ba0b184a02b1e03f536b1d8f02605ff06e99
aaron-adimverse <aaron@adimverse.com>
2025-03-05 10:53:38 -0700

    feat(dev): Add client-atlas-twitter to dev watch list

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -74,7 +74,7 @@ if [ ! -d "$PACKAGES_DIR" ]; then
 fi
 
 # List of working folders to watch (relative to $PACKAGES_DIR)
-WORKING_FOLDERS=("client-atlas-direct" "plugin-storyteller" "core")
+WORKING_FOLDERS=("client-atlas-direct" "plugin-storyteller" "client-atlas-twitter" "core")
 
 # Initialize an array to hold package-specific commands
 COMMANDS=()
